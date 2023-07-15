a92f957eec287a04c35186222325e8fa34e249b5
Suryateja Challa <challasuryateja29@gmail.com>
2023-07-15 23:06:34 +0530

    Completed header for convolution.

--- src/headers_with_implementation/kernel_convolution.hpp
+++ src/headers_with_implementation/kernel_convolution.hpp
@@ -10,8 +10,19 @@ struct Pixel    {
 class kernel_convolution    {
     public:
         kernel_convolution(const std::vector<std::vector<Pixel>>& inputImage);  // Constructor
-              
 
+        std::vector<std::vector<Pixel>> mean_blur();
+        std::vector<std::vector<Pixel>> gaussian_blur();
+        std::vector<std::vector<Pixel>> edge_detection();
+        std::vector<std::vector<Pixel>> sharpen();
+        std::vector<std::vector<Pixel>> grayscale();
+        std::vector<std::vector<Pixel>> blackhat_filter();
+        std::vector<std::vector<Pixel>> apply_threshold(int thresh);              
+
+    private:
+        std::vector<std::vector<Pixel>> image;
+
+        std::vector<std::vector<Pixel>> convolve(const std::vector<std::vector<float>>& kernel);
 };
 
 #endif
