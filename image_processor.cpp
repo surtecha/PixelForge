e3c63eee3a11ff4243be7d8c334393b3e08c20f9
Suryateja Challa <challasuryateja29@gmail.com>
2023-07-15 22:41:31 +0530

    Program reads images and extracts RGB from pixels.

--- image_processor.cpp
+++ image_processor.cpp
@@ -3,8 +3,28 @@
 #include "kernel_convolution.hpp"
 
 int main()  {
+    std::string filename = "imagefile.ppm";
+    std::vector<std::vector<Pixel>> pixels = read_image::read_PPM(filename);
 
-    
+    if(pixels.empty()) {
+        std::cerr << "Error reading image file." < std::endl;
+        return 1;
+    }
+
+
+    // Accessing individual pixels' RGB values
+    for(size_t i=0; i<pixels.size(); i++)   {
+        for(size_t j=0; j<pixels[i].size(); j++)    {
+            
+            const Pixel& pixel = pixels[i][j];
+            unsigned char 
+                            red   = pixel.red,
+                            green = pixel.green,
+                            blue  = pixel.blue;
+            
+        }
+    }
+
 
     return 0;
 }
