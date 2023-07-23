1d7de991321ae5f14d6bbd5b11258c8229eb322f
suryateja-challa <challasuryateja29@gmail.com>
2023-07-23 23:18:54 +0530

    Adding new files for reading images

---
+++ src/Image.h
@@ -0,0 +1,19 @@
+#include <cstdint>
+#include <stdlib.h>
+
+struct Image {
+    uint8_t* data = nullptr;
+    size_t size = 0; 
+    int w;
+    int h;
+    int channels;
+
+    Image(const char* filename);    // Accepts a filename
+    Image(int w, int h, int channels);  // Creating a blank black image
+    Image(const Image& img);  // Copy images
+    ~Image();   // Destructor
+
+    bool read(const char* filename);    // Read a file
+    bool write(const char* filename);   // Write a file
+
+};
