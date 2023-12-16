5f9796a889d2717d095eaefe3b707eaeb0737802
surtecha <suryatejachalla.dev@gmail.com>
2023-12-16 11:36:59 +0530

    Some minor changes

--- test.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  test.cpp
-//  
-//
-//  Created by Suryateja Challa on 15/12/23.
-//
-
-#include "test.hpp"
--- test2.cpp
+++
@@ -1,8 +0,0 @@
-//
-//  test2.cpp
-//  
-//
-//  Created by Suryateja Challa on 15/12/23.
-//
-
-#include <stdio.h>
